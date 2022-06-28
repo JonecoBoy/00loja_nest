9c8191b8fe51a0c4cde17fd984c6610fe8b78d28
Joneco <joneco@hotmail.com>
2022-06-27 23:57:29 -0300

    create all api rest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, UsersModule, ProductsModule, OrdersModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
